5977cc33a3cdd535fa2b523d4af79a04199ebad6
Dinesh4953 <dinu08642@gmail.com>
2025-10-10 09:22:52 +0530

    Update web.py

--- web.py
+++ web.py
@@ -18,7 +18,7 @@ chrome_options.add_experimental_option("detach", True)
 chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 chrome_options.add_argument("--log-level=3")
 chrome_options.add_argument("--window-size=1920,1080")
-# chrome_options.add_argument("--headless")  # Use new headless mode
+chrome_options.add_argument("--headless")  # Use new headless mode
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
